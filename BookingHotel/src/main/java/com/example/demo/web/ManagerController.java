f84bf10f3f367ce136415b302d6e3b431950e20c
0987196951 <64188933+0987196951@users.noreply.github.com>
2022-05-29 03:35:50 +0700

    123
    123

--- BookingHotel/src/main/java/com/example/demo/web/ManagerController.java
+++ BookingHotel/src/main/java/com/example/demo/web/ManagerController.java
@@ -1,5 +1,13 @@
 package com.example.demo.web;
 
-public class ManagerController {
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.data.ManagerRepository;
+
+@RestController
+@RequestMapping(path="/manager", produces = "application/json")
+public class ManagerController {
+	private ManagerRepository managerRepository;
+	private
 }
